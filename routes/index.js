38d4379bbcae13a936dc28d1a4a50b89256b4423
Jiapei Liang <liang98@purdue.edu>
2016-01-30 21:17:59 -0500

    Add Routes for pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (app) {
 
-module.exports = router;
+	// Routes
+	// Home Page
+	app.get('/', function(req, res) {
+	  res.render('index', { title: 'Home' });
+	});
+
+	// Register Page
+	app.get('/register', function (req, res) {
+		res.render('register', {title: 'Register'});
+	});
+
+	app.post('/register', function (req, res) {
+
+	});
+
+	// Login Page
+	app.get('/login', function (req, res) {
+		res.render('login', {title: 'Login'});
+	});
+
+	app.post('/login', function (req, res) {
+
+	});
+
+
+	// Post Page
+	app.get('/post', function (req, res) {
+		res.render('post', {title: 'Post'});
+	});
+
+	app.post('/post', function (req, res) {
+
+	});
+
+	// Logout
+	app.get('/logout', function (req, res) {
+
+	});
+};
+
+// module.exports = router;
